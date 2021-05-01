4045288a8975ff4f795c2fdd4d16580f173a7280
jan_chyczynski <jasiek.ch16@gmail.com>
2021-05-01 22:21:53 +0200

    update sensor

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ api = Api(app)
 sensors_post_args = reqparse.RequestParser()
 sensors_post_args.add_argument("address", type=str, help="The address of the sensor is required", required=True)
 sensors_post_args.add_argument("owner", type=str, help="The name of the owner of the sensor is required", required=True)
+sensor_patch_args = reqparse.RequestParser()
+sensor_patch_args.add_argument("address", type=str)
+sensor_patch_args.add_argument("owner", type=str)
 
 sensors = {}
-
 
 def assert_sensor_exists(id):
     if id not in sensors:
@@ -22,6 +24,16 @@ class Sensor_res(Resource):
     def get(self, id):
         assert_sensor_exists(id)
         return sensors[id].__dict__()
+
+    def patch(self, id):
+        assert_sensor_exists(id)
+        args = dict(sensor_patch_args.parse_args())
+        print(args)
+        if "address" in args and args["address"] is not None:
+            sensors[id].address = args["address"]
+        if "owner" in args and args["owner"] is not None:
+            sensors[id].owner = args["owner"]
+        return "", 204
 
     def delete(self, id):
         assert_sensor_exists(id)
@@ -47,4 +59,4 @@ api.add_resource(Sensor_res, "/sensor/<int:id>")
 api.add_resource(Sensors_res, "/sensors")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
