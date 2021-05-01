85b63071ef99a649d416378b7d80637ab733358b
jan_chyczynski <jasiek.ch16@gmail.com>
2021-05-01 20:45:40 +0200

    add new sensor

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from sensor import Sensor
 
 app = Flask(__name__)
 api = Api(app)
+
+sensors_post_args = reqparse.RequestParser()
+sensors_post_args.add_argument("address", type=str, help="The address of the sensor is required", required=True)
+sensors_post_args.add_argument("owner", type=str, help="The name of the owner of the sensor is required", required=True)
 
 sensors = {}
 
@@ -14,6 +18,11 @@ class Sensors_res(Resource):
     def get(self):
         return {key: sensors[key].__dict__() for key in sensors.keys()}
 
+    def post(self):
+        args = sensors_post_args.parse_args()
+        sensors[self.counter] = Sensor(address=args["address"], owner=args["owner"])
+        self.counter += 1
+        return {"message": "posted", "id": self.counter-1}, 201
 
 api.add_resource(Sensors_res, "/sensors")
 
